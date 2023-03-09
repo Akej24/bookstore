7b57df64aba3cde74e42eabb651f580e35f506e3
Akej24 <aleks.skotarek7@wp.pl>
2023-03-09 14:26:30 +0100

    Swagger

---
+++ bookstore/src/main/java/com/bookstoreapplication/bookstore/user/account/UserResponse.java
@@ -0,0 +1,22 @@
+package com.bookstoreapplication.bookstore.user.account;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+class UserResponse {
+
+    private String email;
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private UserRole role;
+    private Double availableFunds;
+    private Integer purchasedBooks;
+
+}
