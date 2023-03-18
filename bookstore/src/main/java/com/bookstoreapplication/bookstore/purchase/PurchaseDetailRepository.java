c25724f9051f30d3a7ccaafac759841763f63597
Akej24 <aleks.skotarek7@wp.pl>
2023-03-18 20:15:32 +0100

    Aspect with after, before and execution time added for each controller

--- bookstore/src/main/java/com/bookstoreapplication/bookstore/purchase/PurchaseDetailRepository.java
+++ bookstore/src/main/java/com/bookstoreapplication/bookstore/purchase/PurchaseDetailRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PurchaseDetailRepository extends JpaRepository<PurchaseDetail, Long> {
+interface PurchaseDetailRepository extends JpaRepository<PurchaseDetail, Long> {
 
     void deleteByPurchasePurchaseId(long purchaseId);
 
--- bookstore/src/main/java/com/bookstoreapplication/bookstore/purchase/PurchaseRepository.java
+++ bookstore/src/main/java/com/bookstoreapplication/bookstore/purchase/PurchaseRepository.java
@@ -1,18 +1,15 @@
 package com.bookstoreapplication.bookstore.purchase;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
     List<Purchase> findByUserUserIdAndPurchaseStatus(long userId, PurchaseStatus status);
 
     List<Purchase> findByUserUserId(long userId);
 
-//    @Query("SELECT DISTINCT p FROM Purchase p JOIN FETCH p.purchaseDetails")
-//    List<Purchase> findAllPurchasesWithDetailsByUserId(long userId);
 }
