3b5043b21529db4fad4e64afd4bb203aeeb59ea2
Akej24 <aleks.skotarek7@wp.pl>
2023-03-30 16:16:33 +0200

    fixed sql insert passwords

--- bookstore/src/main/resources/database/02-insert-startup-data.sql
+++ bookstore/src/main/resources/database/02-insert-startup-data.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 --changeset akej:1
 INSERT INTO users (email, username, password, name, surname, date_of_birth, role, available_funds, locked, enabled, created_on)
-VALUES ('admin@test.com', 'admin', 'Admin123!', 'admin', 'admin', '2023-01-01', 'ADMIN', 1000.00, false, true, now());
+VALUES ('admin@test.com', 'admin', '$2a$12$N/gJODsnrfOIiaroZYg80uxunYfTCQdqq19/qYX9AngKPFvECBEmW', 'admin', 'admin', '2023-01-01', 'ADMIN', 1000.00, false, true, now());
 INSERT INTO users (email, username, password, name, surname, date_of_birth, role, available_funds, locked, enabled, created_on)
-VALUES ('user@test.com', 'user', 'User123!', 'user', 'user', '2023-01-01', 'USER', 1000.00, false, true, now());
+VALUES ('user@test.com', 'user', '$2a$12$fVgJMoqbMukVKh55j3TT4OyQNIOr09DXdeRePigLNyBcO3SHsIutW', 'user', 'user', '2023-01-01', 'USER', 1000.00, false, true, now());
 
 --changeset akej:2
 INSERT INTO books (title, author, release_date, number_of_pages, status, available_pieces, price, created_on)
