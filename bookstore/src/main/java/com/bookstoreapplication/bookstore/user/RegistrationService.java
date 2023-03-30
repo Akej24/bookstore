cabc310c6f45e5eccaa54df6d0c9bf1ed1bd528a
Akej24 <aleks.skotarek7@wp.pl>
2023-03-30 16:16:57 +0200

    moved files to account package

---
+++ bookstore/src/main/java/com/bookstoreapplication/bookstore/user/RegistrationService.java
@@ -0,0 +1,58 @@
+package com.bookstoreapplication.bookstore.user;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+
+@Service
+@Validated
+@AllArgsConstructor
+class RegistrationService {
+
+    private final UserRepository userRepository;
+    private final RegistrationValidator registrationValidator;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Logger logger = LoggerFactory.getLogger(RegistrationService.class);
+
+    @Transactional
+    void registerUserIfPasswordValidAndEmailNotTaken(@Valid RegistrationRequest request) {
+        validateEmail(request);
+        validatePassword(request);
+        User modelToSave = createUserFromRequest(request);
+        userRepository.save(modelToSave);
+        logger.info("Successfully registered with email {}", request.getEmail());
+    }
+
+    private void validatePassword(RegistrationRequest request) {
+        if(!registrationValidator.validatePassword(request.getPassword())){
+            logger.warn("Unsuccessfully registered - the password must contain 8-16 characters, one lowercase letter, one uppercase letter, a special character and a number");
+            throw new IllegalArgumentException("Unsuccessfully registered - the password must contain 8-16 characters, one lowercase letter, one uppercase letter, a special character and a number");
+        }
+    }
+
+    private void validateEmail(RegistrationRequest request) {
+        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+            logger.warn("Unsuccessfully registered - email must be not taken");
+            throw new IllegalArgumentException("Unsuccessfully registered - email must be not taken");
+        }
+    }
+
+    private User createUserFromRequest(RegistrationRequest request) {
+        return User.builder()
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .password(bCryptPasswordEncoder.encode(request.getPassword()))
+                .name(request.getName())
+                .surname(request.getSurname())
+                .dateOfBirth(request.getDateOfBirth())
+                .role(request.getRole())
+                .build();
+    }
+
+}
