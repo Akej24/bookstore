0732b06d611bc687767fe35aa784ca96f319302d
Akej24 <aleks.skotarek7@wp.pl>
2023-05-27 18:43:54 +0200

    extracted duplicated code

--- bookstore/adapters/src/main/java/com.bookstoreapplication.bookstore/auth/JwtService.java
+++ bookstore/adapters/src/main/java/com.bookstoreapplication.bookstore/auth/JwtService.java
@@ -3,9 +3,11 @@ package com.bookstoreapplication.bookstore.auth;
 import com.bookstoreapplication.bookstore.auth.exception.JwtNofFoundInRequestHeaderException;
 import com.bookstoreapplication.bookstore.auth.exception.UserEmailHasNotBeenFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,16 +17,23 @@ public class JwtService {
     private final SecuredUserRepository securedUserRepository;
 
     public long extractUserIdFromRequest(HttpServletRequest request){
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new JwtNofFoundInRequestHeaderException();
-        }
-        String jwt = authHeader.substring(7);
+        String jwt = extractJwtFromRequest(request);
         String email = jwtManager.getUsernameFromToken(jwt);
         SecuredUser securedUser = securedUserRepository.findByUserEmailEmail(email)
                 .orElseThrow(UserEmailHasNotBeenFoundException::new);
         return securedUser.getUserId();
-
     }
 
+    public List<GrantedAuthority> extractUserRoleFromRequest(HttpServletRequest request){
+        String jwt = extractJwtFromRequest(request);
+        return jwtManager.getUserRoleFromToken(jwt);
+    }
+
+    private static String extractJwtFromRequest(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new JwtNofFoundInRequestHeaderException();
+        }
+        return authHeader.substring(7);
+    }
 }
