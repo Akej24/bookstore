77f3e4498b8ca30769b04edb6a5051a9ae4b0cc1
Akej24 <aleks.skotarek7@wp.pl>
2023-06-08 15:50:16 +0200

    fixed response for user registration

--- bookstore/adapters/src/main/java/com.bookstoreapplication.bookstore/user/UserJsonCommand.java
+++ bookstore/adapters/src/main/java/com.bookstoreapplication.bookstore/user/UserJsonCommand.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -44,8 +45,15 @@ class UserJsonCommand implements JsonEncodable {
                 JsonDecoder.field(json, "lastName", lastName ->
                         new LastName(JsonDecoder.string(lastName))),
 
-                JsonDecoder.field(json, "dateOfBirth", dateOfBirth ->
-                        new DateOfBirth(LocalDate.parse(JsonDecoder.string(dateOfBirth), LOCAL_DATE_FORMATTER))),
+                JsonDecoder.field(json, "dateOfBirth", dateOfBirth -> {
+                    LocalDate date;
+                    try {
+                        date = LocalDate.parse(JsonDecoder.string(dateOfBirth), LOCAL_DATE_FORMATTER);
+                    } catch(DateTimeParseException dateTimeParseException) {
+                        throw new IllegalArgumentException("Invalid date of birth");
+                    }
+                    return new DateOfBirth(date);
+                }),
 
                 JsonDecoder.field(json, "role", role -> UserRole.valueOf(JsonDecoder.string(role)))
         );
